d5f5ba34ce8128c09686c128adde37501d954638
Saradiyal <manujayapremathilaka@gmail.com>
2019-08-02 19:54:11 +0530

    Updated Login and Registration

--- app/src/main/java/com/example/manujayapremathilaka/market/MarketHome.java
+++ app/src/main/java/com/example/manujayapremathilaka/market/MarketHome.java
@@ -4,18 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MarketHome extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_home);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(MarketHome.this, CustomerRegistration.class);
+                startActivity(register);
+            }
+        });
     }
 
     public void pushRegisterButton(View view){
-        Intent register = new Intent(MarketHome.this, CustomerRegistration.class);
-        startActivity(register);
+          Intent register = new Intent(MarketHome.this, CustomerRegistration.class);
+          startActivity(register);
     }
 
     public void pushLoginButton(View view){
