6e8221b93e5526d2f7bf03e24ee46d0cdf871a04
Saradiyal <manujayapremathilaka@gmail.com>
2019-08-03 23:04:00 +0530

    * Intergrated with 'delivery'

--- app/src/main/java/com/example/manujayapremathilaka/market/Cart.java
+++ app/src/main/java/com/example/manujayapremathilaka/market/Cart.java
@@ -1,7 +1,11 @@
 package com.example.manujayapremathilaka.market;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class Cart extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class Cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+    }
+
+    public void onOrderListButtonPush(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Order Request Sent";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.BOTTOM,0,0);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/manujayapremathilaka/market/EmployeeHome.java
+++ app/src/main/java/com/example/manujayapremathilaka/market/EmployeeHome.java
@@ -1,7 +1,9 @@
 package com.example.manujayapremathilaka.market;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmployeeHome extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EmployeeHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home);
+    }
+
+    public void onDeliveryButtonPushed(View view){
+        Intent delivery = new Intent(EmployeeHome.this, SignUpHome.class);
+        startActivity(delivery);
     }
 }
