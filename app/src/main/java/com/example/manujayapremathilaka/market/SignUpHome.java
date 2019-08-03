e74f971e134fd46a2f7c62f4cd2c85f020bab921
Kusal Chathuranga <kusalchathuranga2@gmail.com>
2019-08-03 20:06:01 +0530

    aaa

---
+++ app/src/main/java/com/example/manujayapremathilaka/market/SignUpHome.java
@@ -0,0 +1,20 @@
+package com.example.manujayapremathilaka.market;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_home);
+    }
+
+    public void Loadregister(View view) {
+        Intent register = new Intent(SignUpHome.this, Register.class);
+        startActivity(register);
+    }
+}
