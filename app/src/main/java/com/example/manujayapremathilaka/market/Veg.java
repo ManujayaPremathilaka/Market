4f94b354d2b630895719b02294835bb92d073c50
Ramishka <dyasasmiliyanage@gmail.com>
2019-09-07 12:56:56 +0530

    Insert Iteam Interface

--- app/src/main/java/com/example/manujayapremathilaka/market/Veg.java
+++ app/src/main/java/com/example/manujayapremathilaka/market/Veg.java
@@ -1,7 +1,9 @@
 package com.example.manujayapremathilaka.market;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Veg extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Veg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_veg);
+    }
+    public void InsertIeam(View view){
+        Intent newiteam = new Intent(Veg.this, InsertItem.class);
+        startActivity(newiteam);
     }
 }
